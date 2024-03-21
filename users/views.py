a906d14a4bf8ddb163827864459ed14e56c4f221
Av3roRR <andrey_emelyanov_2005@mail.ru>
2024-03-21 13:39:39 +0300

    Создали форму для авторизации пользователя настроили отображения плашки при неправильном вводе данных

--- users/views.py
+++ users/views.py
@@ -1,9 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+        print(form.errors)
+    else:
+        form = UserLoginForm()        
+            
     context = {
         'title': 'Home - авторизация',
+        'form': form,
+        
     }
 
     return render(request, 'users/login.html', context=context)
@@ -28,3 +48,4 @@ def profile(request):
 
 def logout(request):
     ...
+    
