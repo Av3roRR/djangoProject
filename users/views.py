fe3562f11bfa19a04c008de1c6452aa86f4dd699
Av3roRR <andrey_emelyanov_2005@mail.ru>
2024-03-26 20:33:41 +0300

    настройка профиля пользователя и всплывающие плашки при регистрации и выходе с акканта

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+
+from traitlets import Instance
+
+
+
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request):
@@ -15,6 +21,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{user.username}, Вы успешно зарегестрированы и вошли в аккаунт")
                 return HttpResponseRedirect(reverse('main:index'))
         print(form.errors)
     else:
@@ -47,17 +54,28 @@ def registration(request):
 
     return render(request, 'users/registration.html', context=context)
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Изменения в профиле вступили в силу")
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = ProfileForm(instance=request.user) 
+    
     context = {
         'title': 'Home - кабинет',
+        'form': form,
     }
 
     return render(request, 'users/profile.html', context=context)
 
 
-
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из акканта")
     auth.logout(request)
     
     return redirect(reverse('main:index'))
